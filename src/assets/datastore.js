ef2d18650414fffa1417bbfce24ebdeb29a86575
madarkalgaurav <gaurav@bellabona.com>
2020-10-03 12:37:54 +0530

    projects page, created reusable component, desgining projects page

--- src/assets/datastore.js
+++ src/assets/datastore.js
@@ -2,8 +2,11 @@ const baseurl = "../../assets/img/project/";
 export const projects = [
   {
     projectTitle: "Hush - Android Application",
-    projectDetails:
+    projectType: "A N D R O I D",
+    projectOverview:
       "Introducing Hush, an event scheduler and peer activity viewer application",
+    projectDetails:
+      "Did your phone ever ring when you didn't want it to? Well worry no more, We have got just the thing for you. Hush comes with a very user friendly and easy to use UI, we follow the policy of Less clicks more work. With Hush you will be able to Setup multiple profiles of Quite-Hours(one for driving, one for sleeping, one for meetings etc). Set status based on your availability, which can be viewed by your contacts. There are many more features to explore, click below to learn more",
     projectLink:
       "https://play.google.com/store/apps/details?id=org.hushapps.hush",
     projectImageName: baseurl + "1.jpg",
@@ -12,16 +15,22 @@ export const projects = [
   },
   {
     projectTitle: "Personal Portfolio",
-    projectDetails: "Personal portfolio website developed using react",
+    projectType: "W E B",
+    projectOverview: "Personal portfolio website developed using react",
     projectLink: "https://deepthi-murthy.web.app",
+    projectDetails:
+      "Did your phone ever ring when you didn't want it to? Well worry no more, We have got just the thing for you. Hush comes with a very user friendly and easy to use UI, we follow the policy of Less clicks more work. With Hush you will be able to Setup multiple profiles of Quite-Hours(one for driving, one for sleeping, one for meetings etc). Set status based on your availability, which can be viewed by your contacts. There are many more features to explore, click below to learn more",
     projectImageName: "./img/project/2.jpg",
     username: "gauravm",
     id: 2,
   },
   {
     projectTitle: "Network Connections",
-    projectDetails:
+    projectType: "O S S",
+    projectOverview:
       "DotNet standard application developed for the sole purpose of simplifying fetching network details",
+    projectDetails:
+      "Did your phone ever ring when you didn't want it to? Well worry no more, We have got just the thing for you. Hush comes with a very user friendly and easy to use UI, we follow the policy of Less clicks more work. With Hush you will be able to Setup multiple profiles of Quite-Hours(one for driving, one for sleeping, one for meetings etc). Set status based on your availability, which can be viewed by your contacts. There are many more features to explore, click below to learn more",
     projectLink: "https://www.nuget.org/packages/NetworkConnections/",
     projectImageName: "../../assets/img/project/3.jpg",
     username: "gauravm",
@@ -29,8 +38,11 @@ export const projects = [
   },
   {
     projectTitle: "Smart Home Energy Management",
-    projectDetails:
+    projectType: "D A T A A N A L Y T I C S",
+    projectOverview:
       "Smart Home Energy Management is a system that provides monitoring energy usage in user's house and inform the user about his/her usage patterns, spikes and surges.",
+    projectDetails:
+      "Did your phone ever ring when you didn't want it to? Well worry no more, We have got just the thing for you. Hush comes with a very user friendly and easy to use UI, we follow the policy of Less clicks more work. With Hush you will be able to Setup multiple profiles of Quite-Hours(one for driving, one for sleeping, one for meetings etc). Set status based on your availability, which can be viewed by your contacts. There are many more features to explore, click below to learn more",
     projectLink: "",
     projectImageName: "../../assets/img/project/4.jpg",
     username: "gauravm",
@@ -38,8 +50,11 @@ export const projects = [
   },
   {
     projectTitle: "Traffic Lights Automation System",
-    projectDetails:
+    projectType: "E M B E D D E D",
+    projectOverview:
       "This project describes the use of wireless communications for relieving ambulance from traffic congestion in urban areas.",
+    projectDetails:
+      "Did your phone ever ring when you didn't want it to? Well worry no more, We have got just the thing for you. Hush comes with a very user friendly and easy to use UI, we follow the policy of Less clicks more work. With Hush you will be able to Setup multiple profiles of Quite-Hours(one for driving, one for sleeping, one for meetings etc). Set status based on your availability, which can be viewed by your contacts. There are many more features to explore, click below to learn more",
     projectLink: "",
     projectImageName: "../../assets/img/project/5.jpg",
     username: "gauravm",
--- src/js/components/project.jsx
+++ src/js/components/project.jsx
@@ -1,33 +1,99 @@
 import React from "react";
 import Carousel from "react-bootstrap/Carousel";
+import { OverlayTrigger, Tooltip } from "react-bootstrap";
 import { projects } from "../../assets/datastore";
 import "../../css/bootstrap.min.css";
 import { images } from "../../assets/img/project/1.jpg";
+let mediaScreen = false;
+const renderTooltip = (props) => (
+  <Tooltip id="button-tooltip" className="tool_tip" {...props}>
+    View Next
+  </Tooltip>
+);
+
 function carouselItem(project) {
+  var v = window.matchMedia("(max-width: 600px)");
+  mediaScreen = v.matches;
   // const url = project.projectImageName;
   console.log(images);
   return (
     <Carousel.Item>
       <a href={"#project" + project.id}>
-        <img
-          className="slideshow__img"
-          src={require("../../assets/img/project/" + project.id + ".jpg")}
-          alt="Project Image"
-        />
+        {!mediaScreen ? (
+          <img
+            className="slideshow__img"
+            src={require("../../assets/img/project/" + project.id + ".jpg")}
+            alt="Project Image"
+          />
+        ) : (
+          <img
+            className="slideshow__img"
+            src={require("../../assets/img/project/alt/" + project.id + ".jpg")}
+            alt="Project Image"
+          />
+        )}
       </a>
       <Carousel.Caption>
         <div className="projectTitle">{project.projectTitle}</div>
-        <div className="projectDetails">{project.projectDetails}</div>
+        <div className="projectDetails">{project.projectOverview}</div>
       </Carousel.Caption>
     </Carousel.Item>
   );
 }
 
+function projectContainer(project, len) {
+  return (
+    <div className="container" id={"project" + project.id}>
+      <div className="grid-container__projects">
+        <div className="grid-item__projects">
+          <div className="tag__wrapper">
+            <div className="tag">{project.projectType}</div>
+          </div>
+        </div>
+        <div className="grid-item__projects">
+          <div className="project__info">
+            <p className="__title">{project.projectTitle}</p>
+            {/* <div className="grid-container__projectinfo"> */}
+
+            <p className="project__desc">{project.projectDetails}</p>
+            <p className="left-wrapper">
+              <a href="/about" class="btn learnmore-button">
+                More Info
+              </a>
+            </p>
+            {project.id !== len ? (
+              <p className="viewnext">
+                <OverlayTrigger
+                  placement="right"
+                  delay={{ show: 250, hide: 400 }}
+                  overlay={renderTooltip}
+                >
+                  <a href={"#project" + (project.id + 1)}>
+                    <img
+                      className="viewnext__img"
+                      src={require("../../assets/img/icons/down.png")}
+                      alt=""
+                    />
+                  </a>
+                </OverlayTrigger>
+              </p>
+            ) : (
+              <div></div>
+            )}
+          </div>
+          {/* </div> */}
+        </div>
+      </div>
+    </div>
+  );
+}
 export default function Project() {
   return (
     <div>
       <div className="container" id="main">
-        <h1 className="heading">Projects</h1>
+        <h1 className="heading">
+          <span className="name">PRO</span>JECTS
+        </h1>
         <Carousel className="slideshow">
           {projects.map((project) => {
             return carouselItem(project);
@@ -41,45 +107,9 @@ export default function Project() {
       <a href="/about" class="btn learnmore-button">
         Contact Me
       </a>
-      <div className="container" id="project1">
-        <div className="project__info">
-          <p className="__title">Hush - Android Application</p>
-          <div className="grid-container__projectinfo">
-            <div className="grid-item">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-              reprehenderit in voluptate velit esse cillum dolore eu fugiat
-              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-              sunt in culpa qui officia deserunt mollit anim id est
-              laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-              sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-              Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-              reprehenderit in voluptate velit esse cillum dolore eu fugiat
-              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-              sunt in culpa qui officia deserunt mollit anim id est laborum.
-              <p className="left-wrapper">
-                <a href="/about" class="btn learnmore-button">
-                  More Info
-                </a>
-              </p>
-            </div>
-            <div className="grid-item">
-              <img
-                src={require("../../assets/img/icons/android.png")}
-                className="project__image"
-              ></img>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className="container" id="project2"></div>
-      <div className="container" id="project3"></div>
-
-      <div className="container" id="project4"></div>
-      <div className="container" id="project5"></div>
+      {projects.map((project) => {
+        return projectContainer(project, projects.length);
+      })}
     </div>
   );
 }

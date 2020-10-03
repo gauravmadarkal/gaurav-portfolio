4b0cb99f18a2ccd107ca77c9a81b492620f00eec
madarkalgaurav <gaurav@bellabona.com>
2020-10-03 19:52:27 +0530

    added work page

--- src/js/components/work.jsx
+++ src/js/components/work.jsx
@@ -1,8 +1,43 @@
 import React from "react";
+import images from "../../assets/img/work/work-bg-img-2.jpg";
+import { OverlayTrigger, Tooltip } from "react-bootstrap";
+const renderTooltip = (props) => (
+  <Tooltip id="button-tooltip" className="tool_tip" {...props}>
+    View Next
+  </Tooltip>
+);
+
 export default function Work() {
   return (
-    <div className="container" id="work">
-      <h1>Work</h1>
+    <div>
+      <div className="container">
+        <div className="work__container">
+          <div className="work__grid-item1">
+            <img
+              className="work"
+              src={require("../../assets/img/work/work-bg-3.jpg")}
+            ></img>
+          </div>
+          <div className="work__grid-item2">
+            <p className="viewnext">
+              <OverlayTrigger
+                placement="right"
+                delay={{ show: 250, hide: 400 }}
+                overlay={renderTooltip}
+              >
+                <a style={{ textDecoration: "none" }} href={"#project"}>
+                  <p className="button-text">My Work</p>
+                  <img
+                    className="viewnext__img"
+                    src={require("../../assets/img/icons/down.png")}
+                    alt=""
+                  />
+                </a>
+              </OverlayTrigger>
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
